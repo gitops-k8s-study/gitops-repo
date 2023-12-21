1159d5f3d6ea3d233d515ea99bf91c437261adfb
serapple <serinamgil@gmail.com>
2023-12-21 16:26:00 +0900

    [build] yaml파일 git 주소 수정

--- platform/aws/ap-northeast-2/terraform-codes/iac.tf
+++ platform/aws/ap-northeast-2/terraform-codes/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-codes" {
-  source = "git@github.com:<Github Profile명>/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
+  source = "git@github.com/gitops-k8s-study/platform-repo.git//aws/ap-northeast-2/terraform-codes?ref=main"
 }
